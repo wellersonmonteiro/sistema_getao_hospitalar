7a1d42326634a828a5b91d6d4b8b0f2db48a9323
wellerson <wellersonjava@gmail.com>
2024-04-23 13:09:59 -0300

    update: correção e adequação ao Camalcase

--- src/main/java/br/projeto/appgestaohvida/controller/ControllerListaP.java
+++ src/main/java/br/projeto/appgestaohvida/controller/ControllerListaP.java
@@ -1,24 +1,21 @@
-package br.projeto.appgestaohvida.Controller;
+package br.projeto.appgestaohvida.controller;
 
 import br.projeto.appgestaohvida.model.ListasPacientes;
 import br.projeto.appgestaohvida.model.Paciente;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/listas/A")
+@RequestMapping("/listas/P")
 @CrossOrigin(origins = {"http://127.0.0.1:5500", "https://example.com"}, allowCredentials = "true")//Colocar o endereço do servidor front end
 
-public class ContrellerListaA {
+public class ControllerListaP {
     private int valor = 0;
-    @Autowired
-    private ListasPacientes<Paciente> listaPacientesA; // Instância da lista de pacientes
+    private ListasPacientes<Paciente> listaPacientesP = new ListasPacientes<>(); // Instância da lista de pacientes
 
     @GetMapping
     public String obterPrimeiraSenha() {
-        if (listaPacientesA.getTamanho() > 0) {
-            Paciente primeiroPaciente = listaPacientesA.getPrimeiro();
-            listaPacientesA.excluirPrimeiro();
+        if (listaPacientesP.getTamanho() > 0) {
+            Paciente primeiroPaciente = listaPacientesP.getPrimeiro();
             return "{\"senha\":\""+  primeiroPaciente.getSenha()+"\"}";
         } else {
             return "{\"senha\":\"Sem atendimento\"}";
@@ -27,14 +24,20 @@ public class ContrellerListaA {
 
     @PostMapping
     public String cadastrarNovoPaciente() {
-        String novaSenha = "A" + (valor + 1);
+        String novaSenha = "P" + (valor + 1);
         Paciente novoPaciente = new Paciente(novaSenha); // Cria um novo paciente com a nova senha
-        listaPacientesA.adicionar(String.valueOf(novoPaciente)); // Adiciona o novo paciente à lista
+        listaPacientesP.adicionar(String.valueOf(novoPaciente)); // Adiciona o novo paciente à lista
         valor++; //
 
         return "Paciente cadastrado com senha: " + novaSenha;
     }
-    public boolean temListaA() {
-        return (listaPacientesA.getTamanho() > 0);
+
+    @DeleteMapping
+    public String retirarLista(){
+        listaPacientesP.excluirPrimeiro();
+        return "Paciente removido com sucesso!";
+    }
+    public boolean temListaP() {
+        return (listaPacientesP.getTamanho() > 0);
     }
 }
