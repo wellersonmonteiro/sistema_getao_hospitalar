4dfc02f0af7cc1a180de40b5aa59209cae0be033
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2024-05-01 20:17:32 -0300

    merge versaogab com master

--- src/main/java/br/projeto/appgestaohvida/model/Paciente.java
+++ src/main/java/br/projeto/appgestaohvida/model/Paciente.java
@@ -1,15 +1,16 @@
 package br.projeto.appgestaohvida.model;
 
-
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+@NoArgsConstructor
 @Getter
 @Setter
-
 public class Paciente<TIPO> {
     LocalTime now = LocalTime.now();
 
@@ -19,11 +20,73 @@ public class Paciente<TIPO> {
 
     public Paciente(String senha) {
         this.senha = senha;
+    }
 
+    // Novo construtor que aceita senha e hora
+    public Paciente(String senha, String hora) {
+        this.senha = senha;
+        this.hora = hora;
     }
 
     @Override
     public String toString() {
-        return ""+senha+"";
+        return "" + senha + "";
     }
+
+    // Getters e setters omitidos por brevidade...
+
+    @Getter
+    @JsonProperty("viaAerea")
+    private boolean viaAerea;
+    @Getter
+    @JsonProperty("respiracaoIneficaz")
+    private boolean respiracaoIneficaz;
+    @Getter
+    @JsonProperty("choque")
+    private boolean choque;
+    @Getter
+    @JsonProperty("naoRespondeEstimulo")
+    private boolean naoRespondeEstimulo;
+    @Getter
+    @JsonProperty("emConvulsao")
+    private boolean emConvulsao;
+    @Getter
+    @JsonProperty("dorSevera")
+    private boolean dorSevera;
+    @Getter
+    @JsonProperty("grandeHemorragia")
+    private boolean grandeHemorragia;
+    @Getter
+    @JsonProperty("alteracaoConsciencia")
+    private boolean alteracaoConsciencia;
+    @Getter
+    @JsonProperty("temperaturaAlta")
+    private boolean temperaturaAlta;
+    @Getter
+    @JsonProperty("traumaCraniano")
+    private boolean traumaCraniano;
+    @Getter
+    @JsonProperty("pequenaHemorragia")
+    private boolean pequenaHemorragia;
+    @Getter
+    @JsonProperty("dorModerada")
+    private boolean dorModerada;
+    @Getter
+    @JsonProperty("vomitoPersistente")
+    private boolean vomitoPersistente;
+    @Getter
+    @JsonProperty("temperaturaEntre38e39")
+    private boolean temperaturaEntre38e39;
+    @Getter
+    @JsonProperty("idosoOuGravidaSintomatico")
+    private boolean idosoOuGravidaSintomatico;
+    @Getter
+    @JsonProperty("dorLeve")
+    private boolean dorLeve;
+    @Getter
+    @JsonProperty("nauseas")
+    private boolean nauseas;
+    @Getter
+    @JsonProperty("temperaturaEntre37e38")
+    private boolean temperaturaEntre37e38;
 }
