8d5196ef133d1f39fdbd7533b0c2cfef7a200a19
Gabriel Soares <120689729+GabrielnSoares@users.noreply.github.com>
2024-05-01 19:06:39 -0300

    configuração do post no ControllerConsMainsmain e teste do cliente com arquivo json_teste

--- src/main/java/br/projeto/appgestaohvida/controller/filasconsultorio/ControllerConsMain.java
+++ src/main/java/br/projeto/appgestaohvida/controller/filasconsultorio/ControllerConsMain.java
@@ -1,10 +1,8 @@
 package br.projeto.appgestaohvida.controller.filasconsultorio;
 
+import br.projeto.appgestaohvida.model.Paciente;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consultorio/main")
@@ -42,4 +40,54 @@ public class ControllerConsMain {
             return "{\"senha\":\"Sem atendimento\"}";
         }
     }
+
+    @PostMapping
+    public String adicionarPaciente(@RequestBody Paciente condicoes) {
+        String corFila;
+
+        // Verifica as condicoes do paciente para determinar a cor da fila
+        if (condicoes.isViaAerea() || condicoes.isRespiracaoIneficaz() || condicoes.isChoque() ||
+                condicoes.isNaoRespondeEstimulo() || condicoes.isEmConvulsao()) {
+            corFila = "vermelha";
+        } else if (condicoes.isDorSevera() || condicoes.isGrandeHemorragia() || condicoes.isAlteracaoConsciencia() ||
+                condicoes.isTemperaturaAlta() || condicoes.isTraumaCraniano()) {
+            corFila = "amarela";
+        } else if (condicoes.isPequenaHemorragia() || condicoes.isDorModerada() || condicoes.isVomitoPersistente()) {
+            corFila = "amarela";
+        } else if (condicoes.isTemperaturaEntre38e39() || condicoes.isIdosoOuGravidaSintomatico()) {
+            corFila = "verde";
+        } else if (condicoes.isDorLeve() || condicoes.isNauseas() || condicoes.isTemperaturaEntre37e38()) {
+            corFila = "azul";
+        } else {
+            corFila = "azul"; // Definir uma cor padrao se nao houver correspondencia com as condicoes conhecidas
+        }
+
+        // Adiciona o paciente a fila correspondente
+        switch (corFila) {
+            case "vermelha":
+                // Adicionar a lista vermelha
+                controllerListaVermelha.cadastrarNovoPaciente();
+                break;
+            case "amarela":
+                // Adicionar a lista amarela
+                controllerListaAmarela.cadastrarNovoPaciente();
+                break;
+            case "verde":
+                // Adicionar a lista verde
+                controllerListaVerde.cadastrarNovoPaciente();
+                break;
+            case "azul":
+                // Adicionar a lista azul
+                controllerListaAzul.cadastrarNovoPaciente();
+                break;
+            default:
+
+                break;
+        }
+
+        return "Paciente adicionado com sucesso a fila " + corFila;
+    }
+
+
+
 }
